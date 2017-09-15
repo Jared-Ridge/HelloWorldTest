88b89b8d1aac318b80fac277949eef6f62e49e34
Jared Ridge (63177) <jridge.63177@farnborough.ac.uk>
2017-09-15 13:25:16 +0100

    TEST COMMENTS

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! I am a test!");
     }
+
 }
+// This is a comment
+/* this is another comment.
+It really is quite simple.
+pls work
+ */
